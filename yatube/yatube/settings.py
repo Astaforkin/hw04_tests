c2e98e4d59b46a67ec045c1826e31dcde2a02f5d
Nikita Astaforkin <astaforkin90@gmail.com>
2023-01-19 13:09:58 +0300

    sprint6

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "sorl.thumbnail",
 ]
 
 MIDDLEWARE = [
@@ -139,3 +140,8 @@ EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
 LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'posts:index'
 # LOGOUT_REDIRECT_URL = 'posts:index'
+
+CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -15,6 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+handler404 = 'core.views.page_not_found'
+handler403 = 'core.views.permission_denied'
+# handler403csrf = 'core.views.csrf_failure'
+handler500 = 'core.views.server_error'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +30,7 @@ urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
     path('about/', include("about.urls", namespace='about')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
